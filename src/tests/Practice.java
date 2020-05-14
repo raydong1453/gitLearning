baecbdadd28a7579c9a50d7194c6cde4231b2eab
raydong1453 <reyhan_gezer87@hotmail.com>
2020-05-14 10:27:03 +0200

    add Practice.java

---
+++ src/tests/Practice.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("practice");
+
+        // hallo Tuesday
+        // hi hi
+
+
+
+    }
+
+}
--- src/tests/winter.java
+++ src/tests/winter.java
@@ -3,5 +3,6 @@ package tests;
 public class winter {
     public static void main(String[] args) {
         System.out.println("god bye winter....");
+        // i want to see this line on github
     }
 }
